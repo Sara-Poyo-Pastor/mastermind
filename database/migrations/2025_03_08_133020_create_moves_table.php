96b4f5857547c7770723561b1510f79fc4eea53a
Sara <txara88@hotmail.com>
2025-03-08 14:33:59 +0100

    feat(move)migration

--- database/migrations/2025_03_08_133020_create_moves_table.php
+++ database/migrations/2025_03_08_133020_create_moves_table.php
@@ -1,25 +1,22 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('moves', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('game_id')->constrained('games')->onDelete('cascade');
+            $table->json('guessed_colors'); 
+            $table->json('evaluation');     
+            $table->timestamps(); 
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('moves');
